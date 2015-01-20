5ee7e144500d5b3f5c94904d3a9600c480044730
Tyler Cecchi <thcecchi@gmail.com>
2015-01-20 16:36:41 -0500

    second commit

--- exercises-1.js
+++ exercises-1.js
@@ -104,3 +104,26 @@ var sumOfMatts = getSumOfAccounts(MattsBankAccount);
 var sumOfRobertos = getSumOfAccounts(RobertosBankAccount);
 
 console.log(sumOfMatts - sumOfRobertos); // should calculate to -158
+
+
+/**
+* Hard Mode
+*/
+
+for (i=0; i<100; i++) {
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  }
+
+  else if (i % 3 === 0 && i % 5 === 0 ) {
+    console.log("FizzBuzz");
+  }
+
+  else {
+    console.log(i);
+  }
+}
